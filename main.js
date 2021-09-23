4488a79b0f70a436325ccb3a7ceb5c5f9ab7b07d
shreyas266 <78124801+shreyas266@users.noreply.github.com>
2021-09-22 18:05:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+var player_object="";
+function new_image()
+{
+
+        fabric.Image.fromURL("BirthdayImage.jpg",function(Img){
+            player_object=Img;
+            player_object.scaleToWidth(700);
+            player_object.scaleToHeight(510);
+            player_object.set({
+                top:0,
+                left:0
+            });
+            canvas.add(player_object);
+    
+        });
+    }	
+	
+
+function playSound(){
+x.play();	
+}
